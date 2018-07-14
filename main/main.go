1e031affcfbe18293df6d5e9371475946812cc13
hypany <pany@localhost.localdomain>
2018-07-14 12:53:34 +0800

    测试git

--- main/main.go
+++ main/main.go
@@ -9,6 +9,7 @@ import (
 // var defaultMatrixs = make(map[string]faker.Matrix, 0) // 默认的一些母体，root用户定义的所有母体都是默认母体，普通用户可以向root用户推荐
 // var userMatrixs = make(map[string]faker.Matrix, 0)    // 用户定义的一些母体，可以向其他用户推荐
 
+// 测试git
 func main() {
 	s := [3]byte{1, 2, 3}
 
